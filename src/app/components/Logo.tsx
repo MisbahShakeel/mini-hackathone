e55ebbfb73d620d8fdbf752a6c88e067eb35c26b
MisbahShakeel <misbahshakeeel984@gmail.com>
2025-01-23 21:28:50 +0500

    Add initial project folders

--- src/app/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image"
-
-export default function Images() {
-    return (
-        <div>
-            <Image src="/images.png" alt="img" width={600} height={400} className="w-full"/>
-        </div>
-    )
-}
--- src/sanity/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import products from './products'
-
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [products],
-}
